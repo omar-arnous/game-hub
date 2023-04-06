13fdf8ae4bdfbcdd8c8eb0bafecca4bc22868d27
omar-arnous <omar99.arnous@gmail.com>
2023-04-06 18:39:05 +0300

    Filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -35,7 +35,7 @@ const PlatformIconList = ({ platforms }: Props) => {
       {' '}
       // 1 = 4px in chakra
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
