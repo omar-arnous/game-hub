71f825557cc798b0bfb23d2af8ea82b85cee5fc7
omar-arnous <omar99.arnous@gmail.com>
2023-05-02 21:53:23 +0300

    Refactroing Fetch games with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -20,7 +20,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -34,7 +34,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
