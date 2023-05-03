75793282c728e10a59095b0ea428e63cf6f1e7f2
omar-arnous <omar99.arnous@gmail.com>
2023-05-03 05:37:27 +0300

    Refactor - create lookup hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -2,18 +2,15 @@ import { Heading } from '@chakra-ui/react';
 import { GameQuery } from '../App';
 import useGenres from '../hooks/useGenres';
 import usePlatforms from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGenres();
-  const { data: platforms } = usePlatforms();
-  const genre = genres?.results.find((g) => g.id === gameQuery.genreId);
-  const platform = platforms?.results.find(
-    (p) => p.id === gameQuery.platformId
-  );
+  const genre = usePlatform(gameQuery.genreId);
+  const platform = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,6 +4,7 @@ import { BsChevronDown } from 'react-icons/bs';
 
 // CUSTOM HOOKS
 import usePlatforms, { Platform } from '../hooks/usePlatforms';
+import usePlatform from '../hooks/usePlatform';
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
@@ -12,9 +13,7 @@ interface Props {
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
-  const selectedPlatform = data?.results.find(
-    (p) => p.id === selectedPlatformId
-  );
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
 
