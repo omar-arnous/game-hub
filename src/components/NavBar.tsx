8040b6b828340b4285e15c1494d37c07d9c60c59
omar-arnous <omar99.arnous@gmail.com>
2023-05-06 16:08:28 +0300

    Improving the Game Detail Layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,14 @@ import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} alt="logo" boxSize="60px" />
+      <Link to="/">
+        <Image src={logo} alt="logo" boxSize="60px" />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
